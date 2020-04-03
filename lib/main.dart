5270f48128415109d62ac081d0724a0a62e8dfb5
tomotomy <tomo1999724@gmail.com>
2020-04-03 20:17:43 +0900

    animate3#

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,14 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
   void initState() {
     super.initState();
     controller = AnimationController(duration: const Duration(seconds: 2), vsync: this);
-    animation = Tween<double>(begin: 0, end: 300).animate(controller);
+    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controller.forward();
+        } 
+      });
     controller.forward();
   }
   
